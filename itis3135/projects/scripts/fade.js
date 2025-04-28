fa314d9306978e946e39c57dc5458f66081863a3
TaylorChavis <101995876+TaylorChavis@users.noreply.github.com>
2025-04-27 23:06:20 -0400

    .

---
+++ itis3135/projects/scripts/fade.js
@@ -0,0 +1,21 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(function(entry) {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('visible');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(function(fader) {
+  appearOnScroll.observe(fader);
+});
